b95bf12cb7ed5e8fb7c48052e6405317528214b2
TallenPeli <tallen.pelissero@protonmail.com>
2025-11-29 18:37:01 -0500

    - Fixed mason error in migration - Added cmp-cmdline for command completions - Added option for completions in searching the current buffer

--- nvim/lua/tallenpeli/plugins/cmp.lua
+++ nvim/lua/tallenpeli/plugins/cmp.lua
@@ -2,12 +2,12 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-cmdline", -- completions for vim's cmdline
+		"hrsh7th/cmp-buffer", -- for searching in the current buffer
 		"hrsh7th/cmp-path", -- source for file system paths
 		{
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
-			-- install jsregexp (optional!).
 			build = "make install_jsregexp",
 		},
 		"rafamadriz/friendly-snippets",
@@ -52,9 +52,37 @@ return {
 			},
 		})
 
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		cmp.setup.cmdline("?", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
 		vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
+          set completeopt=menuone,noinsert,noselect
+          highlight! default link CmpItemKind CmpItemMenuDefault
+        ]])
 	end,
 }
--- nvim/lua/tallenpeli/plugins/lspconfig.lua
+++ nvim/lua/tallenpeli/plugins/lspconfig.lua
@@ -4,18 +4,17 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "folke/neodev.nvim", opts = {} },
-
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
+
 	config = function()
+		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local nvim_lsp = require("lspconfig")
-
-		local protocol = require("vim.lsp.protocol")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local on_attach = function(client, bufnr)
-			-- format on save
 			if client.server_capabilities.documentFormattingProvider then
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					group = vim.api.nvim_create_augroup("Format", { clear = true }),
@@ -27,84 +26,47 @@ return {
 			end
 		end
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		mason_lspconfig.setup_handlers({
-			function(server)
-				nvim_lsp[server].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["ts_ls"] = function()
-				nvim_lsp["ts_ls"].setup({
+		mason.setup()
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_ls",
+				"cssls",
+				"tailwindcss",
+				"html",
+				"jsonls",
+				"eslint",
+				"pyright",
+				"clangd",
+				"rust_analyzer",
+				"lua_ls",
+				"omnisharp",
+			},
+		})
+
+		mason_lspconfig.setup({
+			function(server_name)
+				nvim_lsp[server_name].setup({
 					on_attach = on_attach,
 					capabilities = capabilities,
 				})
 			end,
-			["cssls"] = function()
-				nvim_lsp["cssls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["tailwindcss"] = function()
-				nvim_lsp["tailwindcss"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["html"] = function()
-				nvim_lsp["html"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["jsonls"] = function()
-				nvim_lsp["jsonls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["eslint"] = function()
-				nvim_lsp["eslint"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["pyright"] = function()
-				nvim_lsp["pyright"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["clangd"] = function()
-				nvim_lsp["clangd"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
+
 			["rust_analyzer"] = function()
 				nvim_lsp["rust_analyzer"].setup({
 					on_attach = on_attach,
 					capabilities = capabilities,
 					settings = {
 						["rust-analyzer"] = {
-							cargo = {
-								allFeatures = true, -- Enable all features in Cargo.toml
-							},
-							procMacro = {
-								enable = true, -- Enable procedural macros
-							},
+							cargo = { allFeatures = true },
+							procMacro = { enable = true },
 						},
 					},
 				})
 			end,
-			["lua_ls"] = function()
-				nvim_lsp["lua_ls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
+
 			["omnisharp"] = function()
 				nvim_lsp.omnisharp.setup({
 					on_attach = on_attach,
