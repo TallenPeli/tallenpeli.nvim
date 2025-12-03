f4e3bc6379baaaa1a01feed97783d372c826f7df
TallenPeli <tallen.pelissero@protonmail.com>
2025-12-02 22:29:44 -0500

    Added barbar and added more colorscheme options

--- nvim/lua/tallenpeli/plugins/cmp.lua
+++ nvim/lua/tallenpeli/plugins/cmp.lua
@@ -8,6 +8,7 @@ return {
 		{
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
+			-- install jsregexp (optional!).
 			build = "make install_jsregexp",
 		},
 		"rafamadriz/friendly-snippets",
--- nvim/lua/tallenpeli/plugins/colorscheme.lua
+++ nvim/lua/tallenpeli/plugins/colorscheme.lua
@@ -1,15 +1,22 @@
 return {
-	"Mofiqul/vscode.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme vscode")
-	end,
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme vscode")
+		end,
+	},
+	{
 
-    -- "comfysage/evergarden",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    --     vim.cmd("colorscheme evergarden")
-    -- end,
+		"comfysage/evergarden",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+
+		"Mofiqul/adwaita.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 }
